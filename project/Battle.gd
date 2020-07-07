cfb7b9856ede64197b19999c146af6ac38c3be09
Alan Bevier <awbevier@gmail.com>
2020-07-06 21:51:38 -0400

    video 3

--- project/Battle.gd
+++ project/Battle.gd
@@ -1,9 +1,14 @@
 extends Node
 
 onready var enemy = $Enemy
+onready var sword_button = $UI/SwordButton
 
 func _on_SwordButton_pressed():
 	print("Attack!")
-	enemy.hp -= 4
+	if enemy != null:
+		enemy.hp -= 4
 	
-
+func _on_Enemy_died():
+	enemy = null
+	sword_button.hide()
+	pass # Replace with function body.
--- project/Enemy.gd
+++ project/Enemy.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+signal died
+
 var hp = 25 setget set_hp
 
 onready var hp_label = $HPLabel
+onready var animation_player = $AnimationPlayer
 
 func set_hp(value):
 	hp = value
 	hp_label.text = str(hp) + "hp"
+	
+	if hp <= 0:
+		emit_signal("died")
+		queue_free()
+	else:
+		animation_player.play("Shake")
+		yield (animation_player, "animation_finished")
+		animation_player.play("Attack")
 
 
